7e80658d0e503bb41a2eb60b2f2de2c3cc9a599c
Dande Tejaswini <dandetejaswini1329@gmail.com>
2024-02-07 20:41:43 +0530

    import java.util.*; public class Hexa{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         sc.nextLine();         while(a-->0) {             String s = sc.nextLine();             int temp = Integer.parseInt(s,16);             String binary = Integer.toBinaryString(temp);             if(binary.length()%4==0){                 System.out.println(binary);             }             else             {                 int k = binary.length()%4;                 for(int i = 0; i < 4-k; i++){                       System.out.print("0");                 }                 System.out.println(binary);             }         }     } }

---
+++ Hexadecimal_to_Binary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Hexa{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        while(a-->0) {
+            String s = sc.nextLine();
+            int temp = Integer.parseInt(s,16);
+            String binary = Integer.toBinaryString(temp);
+            if(binary.length()%4==0){
+                System.out.println(binary);
+            }
+            else
+            {
+                int k = binary.length()%4;
+                for(int i = 0; i < 4-k; i++){
+                      System.out.print("0");
+                }
+                System.out.println(binary);
+            }
+        }
+    }
+}
